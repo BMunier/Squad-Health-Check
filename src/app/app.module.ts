b6580e216f8701dfc91fc4a082eb6774ae0eee14
Bastien Munier <munier.bastien@gmail.com>
2019-10-04 23:07:01 +0200

    Fisrt commit, creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HealthFormComponent } from './health-form/health-form.component';
+import { MatSliderModule } from '@angular/material';
+import { MaterialModuleModule } from './material-module/material-module.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HealthFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModuleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
